ec61f9f79a777274ddf2d46d18d8b352c770a925
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-14 18:42:22 -0800

    Write bubble_sort function

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,32 @@
 def bubble_sort(array)
+  result = Array.new(array)
+  sorted = false
 
+  while sorted == false do
+    swap_count = 0
+
+    result.each_with_index do |cur_num, cur_index|
+      unless result[cur_index + 1] then
+        next
+      end
+
+      next_num = result[cur_index + 1]
+
+      if cur_num > next_num then
+        result[cur_index] = next_num
+        result[cur_index + 1] = cur_num
+        swap_count += 1
+      end
+    end
+
+    if swap_count == 0 then
+      sorted = true
+    end
+  end
+
+  result
 end
+
+outer_array = [4,3,78,2,0,2]
+
+p bubble_sort(outer_array)
